e39fd714d7648c568259be1598eccf4913ed8736
MrDeys <115146793+MrDeys@users.noreply.github.com>
2026-05-11 17:41:42 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "/api";
+const API_BASE_URL = "https://neuropredict-backend.onrender.com";
 
 const parseData = (data) => {
   if (typeof data === "string") {
